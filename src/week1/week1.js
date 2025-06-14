e8c8afcd65a90075fd843bdfe989ff8c5d7b52bc
jeonjyeon <peacheese17@gmail.com>
2025-06-15 04:14:05 +0900

    ✨ [1주차] 등급 판단 함수 선언 - 등급 판단 함수 (getGrade) - 점수에 따라 등급과 설명이 담긴 객체 반환

--- src/week1/week1.js
+++ src/week1/week1.js
@@ -77,3 +77,32 @@ const getDiscountedPrice = function (originalPrice, discountPercent) {
 };
 
 console.log(getDiscountedPrice(18_700, 20));
+
+// 5. 등급 판단
+// 점수를 전달받아 점수, 등급과 설명을 포함한 객체를 반환하는 함수를 작성합니다.
+
+const getGrade = function (score) {
+  let grade = "";
+  let description = "";
+
+  if (score >= 90 && score <= 100) {
+    grade = "A";
+    description = "매우 우수";
+  } else if (score >= 80) {
+    grade = "B";
+    description = "우수";
+  } else if (score >= 70) {
+    grade = "C";
+    description = "보통";
+  } else if (score >= 60) {
+    grade = "D";
+    description = "미달, 통과 기준 근접";
+  } else {
+    grade = "F";
+    description = "낙제";
+  }
+
+  return { score, grade, description };
+};
+
+console.log(getGrade(87));
