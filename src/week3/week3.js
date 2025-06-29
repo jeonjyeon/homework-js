a5a569325b4f48f5ddb334cc253627ce0d495be9
jeonjyeon <peacheese17@gmail.com>
2025-06-29 10:41:47 +0900

    ✨ [3주차] Off Canvas 및 Accordion 기능 구현 - 메인 메뉴 버튼(햄버거 버튼) 클릭 시, body에 menu-open 클래스 토글 - 이벤트 위임으로 클릭 이벤트 핸들링 - 메뉴 항목 클릭 시, 해당 li에 accordion-open 클래스 토글 - 이미 열린 메뉴가 있으면 닫고, 하나만 열리도록 처리 - 메뉴 외 영역 클릭 시 예외 처리

--- src/week3/week3.js
+++ src/week3/week3.js
@@ -1,1 +1,30 @@
-console.log("잘 연결됨!");
+{
+  const body = document.body;
+  const header = document.querySelector("header");
+  const btnMainMenu = header.querySelector(".btn-main-menu");
+  const menuWrapper = header.querySelector(".menu-wrapper");
+
+  btnMainMenu.addEventListener("click", () => {
+    body.classList.toggle("menu-open");
+  });
+
+  menuWrapper.addEventListener("click", (e) => {
+    const targetButton = e.target.closest("button.accordion-btn");
+    const targetMenu = e.target.closest("li");
+
+    // 이벤트 위임 상황에서, 버튼이나 메뉴 항목 이외의 것을 클릭하였을 경우 함수 실행 중단 시키기 위한 코드
+    if (!targetButton || !targetMenu) return;
+
+    // 현재 열려 있는 메뉴 찾기
+    const currentOpen = menuWrapper.querySelector(".accordion-open");
+
+    // 이미 열린 메뉴가 있고, 그게 지금 클릭한 메뉴가 아니라면 기존에 열려 있던 메뉴 닫기
+    // 두 개가 동시에 열리지 않게 하기 위한 if문
+    if (currentOpen && currentOpen !== targetMenu) {
+      currentOpen.classList.remove("accordion-open");
+    }
+
+    // 토글 (이미 열려 있던 메뉴면 닫고, 아니면 열기)
+    targetMenu.classList.toggle("accordion-open");
+  });
+}
